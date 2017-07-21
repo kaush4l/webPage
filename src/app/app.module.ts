fd034a1790355698aeafe0eba2a599b84c81ee85
kkaush4l <kaushal.beyblader@gmail.com>
2017-07-21 17:12:34 -0400

    works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,44 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { AboutComponent } from './component/about/about.component';
+import { SkillsComponent } from './component/skills/skills.component';
+import { ProjectsComponent } from './component/projects/projects.component';
+import { EducationComponent } from './component/education/education.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutComponent,
+    SkillsComponent,
+    ProjectsComponent,
+    EducationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path : 'about',
+        component : AboutComponent
+      },
+            {
+        path : 'education',
+        component : EducationComponent
+      },
+            {
+        path : 'projects',
+        component : ProjectsComponent
+      },      {
+        path : 'skills',
+        component : SkillsComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
